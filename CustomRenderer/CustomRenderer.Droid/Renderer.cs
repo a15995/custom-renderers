fe680bf0102597cb7f0611082a48cba935280f44
a15995 <123@123.dk>
2016-11-30 10:13:40 +0100

    Custom Renderer pr. app

---
+++ CustomRenderer/CustomRenderer.Droid/Renderer.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using CustomRenderer;
+using CustomRenderer.Droid;
+
+[assembly: ExportRenderer (typeof(MyEntry), typeof(MyEntryRenderer))]
+namespace CustomRenderer.Droid
+{
+    class MyEntryRenderer : EntryRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged (e);
+
+            if (Control != null) {
+                Control.SetBackgroundColor(global::Android.Graphics.Color.LightGreen);
+            }
+        }
+    }
+}
---
+++ CustomRenderer/CustomRenderer.iOS/Renderer.cs
@@ -0,0 +1,26 @@
+﻿using CustomRenderer;
+using CustomRenderer.iOS;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer (typeof(MyEntry), typeof(MyEntryRenderer))]
+namespace CustomRenderer.iOS
+{
+    public class MyEntryRenderer : EntryRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged (e);
+
+            if (Control != null) {
+                // do whatever you want to the UITextField here!
+                Control.BackgroundColor = UIColor.FromRGB(204, 153, 255);
+                Control.BorderStyle = UIKit.UITextBorderStyle.Line;
+            }
+        }
+    }
+}
